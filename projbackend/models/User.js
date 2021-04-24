bc52d13e73f4108639ca2a17dfb421fa6522ad25
faisal <faisal25march99@gmail.com>
2021-04-24 14:25:45 +0530

    Done with Creating a basic Schema for User

---
+++ projbackend/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose();
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 32,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    maxlength: 32,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  userinfo: {
+    type: String,
+    trim: true,
+  },
+  //TODO: comeback here
+  password: {
+    type: String,
+    trime: true,
+    required: true,
+  },
+  salt: String,
+  role: {
+    type: Number,
+    default: 0,
+  },
+  purchases: {
+    type: Array,
+    default: [],
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
