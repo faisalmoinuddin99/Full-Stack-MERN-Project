f71cff9781f93812d22d0269bedae895f1e4b169
faisal <faisal25march99@gmail.com>
2021-04-24 13:48:23 +0530

    Done with Assignment

--- testback/index.js
+++ testback/index.js
@@ -4,7 +4,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.json({ message: "Server connected" });
+});
+
+//Assignment
+app.get("/hitesh", (req, res) => {
+  res.json({ hitesh: "uses instagram" });
+});
+
+app.get("/sign-out", (req, res) => {
+  res.json({ message: "sign out successfully" });
 });
 
 app.listen(port, () => console.log(`Server is running at ${port}...`));
