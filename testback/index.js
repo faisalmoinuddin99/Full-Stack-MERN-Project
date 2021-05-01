cce829c27f7677ce42c218b1701c9d1204f4991e
faisal <faisal25march99@gmail.com>
2021-05-01 11:26:19 +0530

    ProjBackend Middleware Concept

--- testback/index.js
+++ testback/index.js
@@ -7,6 +7,21 @@ app.get("/", (req, res) => {
   res.json({ message: "Server connected" });
 });
 
+const admin = (req, res) => {
+  res.send("this is admin dashboard");
+};
+
+const isAdmin = (req, res, next) => {
+  let checkIsAdmin = true;
+  if (!checkIsAdmin) {
+    console.log("Authentication success....");
+  } else {
+    console.log("Not an Admin");
+  }
+  next();
+};
+app.get("/admin", isAdmin, admin);
+
 //Assignment
 app.get("/hitesh", (req, res) => {
   res.json({ hitesh: "uses instagram" });
