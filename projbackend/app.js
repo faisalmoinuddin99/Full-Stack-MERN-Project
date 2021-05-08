abfcc0ea240b36ae34b7923f088a62fc91d94b3d
faisal <faisal25march99@gmail.com>
2021-05-08 15:34:31 +0530

    What are PARAMS

--- projbackend/app.js
+++ projbackend/app.js
@@ -7,7 +7,9 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
+// My routes
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 
 //DB connection
 mongoose
@@ -28,6 +30,7 @@ app.use(cors());
 
 //My Routes
 app.use("/api", authRoutes);
+app.use("/api", userRoutes);
 
 //PORT
 const port = process.env.PORT || 8000;
